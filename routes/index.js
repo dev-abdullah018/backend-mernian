9d729b18d2737f056d797b3fa86c912ae4807796
dev-abdullah018 <144674090+dev-abdullah018@users.noreply.github.com>
2024-02-24 13:30:37 +0600

    update

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const route = express.Router()
 const apiRouter = require("./api")
 
-route.use("/user", apiRouter)
+route.use(process.env.API_URL, apiRouter)
 
 module.exports = route;
